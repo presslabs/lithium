7c3d34b9878e9fee02df40b858ba93591e3d39ca
vtemian <vladtemian@gmail.com>
2013-11-15 12:30:03 +0200

    Basic filter endpoint

--- plutonium/users/api.py
+++ plutonium/users/api.py
@@ -1,3 +1,5 @@
+from flask.ext.classy import  route
+
 from utils.views import ModelView
 
 from plutonium.users.models import User
@@ -5,3 +7,7 @@ from plutonium.users.models import User
 class UsersView(ModelView):
   model = User
   __exclude__ = ['password']
+
+  @route('/filter/')
+  def filter(self):
+    return "yeye"
