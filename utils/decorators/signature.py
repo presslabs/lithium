55280db4c383bebaf3d18de817bbaf2bab1f76bf
vtemian <vladtemian@gmail.com>
2013-11-16 14:32:35 +0200

    Added signature decorator

---
+++ utils/decorators/signature.py
@@ -0,0 +1,37 @@
+import json
+import hmac
+import hashlib
+from functools import wraps
+
+from flask import request, current_app
+
+from utils.exceptions import HttpUnauthorized, HttpForbidden
+
+def signature(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    if request.method == 'GET':
+      payload = request.args
+
+    key = get_key(payload['client_id'])
+    signature = sign(payload['data'], key)
+
+    if signature != payload['signature']:
+      raise HttpUnauthorized('Invalid signature')
+
+    result = f(*args, **kwargs)
+    return result
+
+  return decorated_function
+
+def sign(message, key):
+  if not isinstance(message, unicode) and not isinstance(message, str):
+    message = json.dumps(message)
+  return hmac.new(key, message, hashlib.sha1).hexdigest()
+
+def get_key(client_id):
+  for client_name, client in current_app.config['CLIENTS'].iteritems():
+    if client['client_id'] == client_id:
+      return client['client_key']
+
+  raise HttpForbidden("Invalid client id")
