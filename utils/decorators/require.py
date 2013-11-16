0b590c2d8b07cf24ceb6d1a06bc9e06f3a540756
vtemian <vladtemian@gmail.com>
2013-11-16 15:39:20 +0200

    improve require decorator

--- utils/decorators/require.py
+++ utils/decorators/require.py
@@ -1,11 +1,30 @@
+import requests
 from functools import wraps
 
-from flask import request
+from flask import request, current_app
 
-def require(permissions):
+from utils.decorators.signature import sign
+
+def require(resource_namespace, permissions, resource_id=None):
   def decorator(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+      if request.method == 'GET':
+        payload = request.args
+
+      client_key = current_app.config['CLIENTS']['plutonium']['client_key']
+      client_id = current_app.config['CLIENTS']['plutonium']['client_id']
+
+      data = []
+      for permission in permissions:
+        data.append({
+          'client_namespace'   : 'app',
+          'client_id'          : payload['client_id'],
+          'resource_namespace' : resource_namespace,
+          'permission'         : permission,
+          'resource_id'        : resource_id or '*'
+        })
+
       result = f(*args, **kwargs)
       return result
     return decorated_function
