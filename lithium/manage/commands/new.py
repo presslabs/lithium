c477d026f6a2f3c871dff8fd3e82a2c84f3c6345
vtemian <vladtemian@gmail.com>
2013-11-29 20:17:08 +0200

    Added color

--- lithium/manage/commands/new.py
+++ lithium/manage/commands/new.py
@@ -5,29 +5,36 @@ from os.path import isdir, join
 from argh.decorators import arg
 from jinja2 import Template
 import yaml
+from colorama import Fore
 
 from elements import all
 
 help = "Bootstrap a service with all you need"
 @arg('name', default=all[randint(0, len(all))], help=help)
 def new(name):
+  print Fore.BLUE + "\nCreating service folder..."
   makedirs(name)
+  print Fore.CYAN + "Done!\n"
 
   with open('layouts/default.yml') as f:
     layout = yaml.load(Template(f.read()).render(app_name=name))
-    create_service(layout, 'templates', name, name)
+    print Fore.RED + "Creating layout..."
+    create_service(1, layout, 'templates', name, name)
 
-  return name
+  print Fore.BLUE + "\nYou're service layout is ready" + Fore.RESET
+  return ""
 
-def create_service(layout, templates_dir, current_dir, app_name):
+def create_service(deep, layout, templates_dir, current_dir, app_name):
   for item in layout:
     if isinstance(item, dict):
       path = "%s/%s" % (current_dir, item.keys()[0])
       makedirs(path)
-      create_service(item[item.keys()[0]], templates_dir, path, app_name)
+      print Fore.BLUE + "  "*deep + "|- %s/" % item.keys()[0]
+      create_service(deep+1, item[item.keys()[0]], templates_dir, path, app_name)
     elif item.endswith("/"):
       path = "%s/%s" % (current_dir, item)
       makedirs(path)
+      print Fore.BLUE + "  "*deep + "|- %s/" % item
     else:
       with open("%s/%s" % (templates_dir, item)) as f:
         content = f.read()
@@ -35,3 +42,4 @@ def create_service(layout, templates_dir, current_dir, app_name):
         content = template.render(app_name=app_name)
       with open("%s/%s" % (current_dir, item), 'w') as f:
         f.write(content)
+        print Fore.CYAN + "  "*deep + "|- %s" % item
