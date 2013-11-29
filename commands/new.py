f9b28118a8990ed6d8669ed40103583cfc9240fc
vtemian <vladtemian@gmail.com>
2013-11-29 19:23:09 +0200

    first command (new)

---
+++ commands/new.py
@@ -0,0 +1,38 @@
+from random import randint
+from os import listdir, chdir, makedirs
+from os.path import isdir, join
+
+from argh.decorators import arg
+from jinja2 import Template
+import yaml
+
+from elements import all
+
+help = "Bootstrap a service with all you need"
+@arg('name', default=all[randint(0, len(all))], help=help)
+def new(name):
+  makedirs(name)
+
+  with open('commands/layouts/default.yml') as f:
+    layout = yaml.load(Template(f.read()).render(app_name=name))
+    create_service(layout, 'commands/templates', name, name)
+
+  return name
+
+def create_service(layout, templates_dir, current_dir, app_name):
+  for item in layout:
+    print item
+    if isinstance(item, dict):
+      path = "%s/%s" % (current_dir, item.keys()[0])
+      makedirs(path)
+      create_service(item[item.keys()[0]], templates_dir, path, app_name)
+    elif item.endswith("/"):
+      path = "%s/%s" % (current_dir, item)
+      makedirs(path)
+    else:
+      with open("%s/%s" % (templates_dir, item)) as f:
+        content = f.read()
+        template = Template(content)
+        content = template.render(app_name=app_name)
+      with open("%s/%s" % (current_dir, item), 'w') as f:
+        f.write(content)
