b2e6db02f980800855a443f94f4200a5de50e100
vtemian <vladtemian@gmail.com>
2013-11-29 19:24:03 +0200

    Simple templates for an app

---
+++ commands/templates/manage.py
@@ -0,0 +1,15 @@
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+from {{ app_name }} import create_app
+from lithium.commands import TestCommand, database
+
+if __name__ == "__main__":
+  manager = Manager(create_app)
+
+  manager.add_option('-c', '--config', dest='config', required=False)
+  manager.add_command('test', TestCommand())
+  manager.add_command('database', database)
+  manager.add_command('migration', MigrateCommand)
+
+  manager.run()
