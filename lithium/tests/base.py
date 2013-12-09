bf2b1026a2fd0ae062db5e9d020dde26bd8f1010
vtemian <vladtemian@gmail.com>
2013-12-09 12:31:31 +0200

    Remove create_app incapsulation

--- lithium/tests/base.py
+++ lithium/tests/base.py
@@ -2,10 +2,6 @@ from flask.ext.testing import TestCase
 
 class BaseTest(TestCase):
   app = None
-  db = None
-
-  def create_app(self):
-    return self.app('local_config.py')
 
   def setUp(self):
     self.db.create_all()
