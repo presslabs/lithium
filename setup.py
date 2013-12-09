e389dfdc05303ddb3462da994753b83687179155
vtemian <vladtemian@gmail.com>
2013-12-09 14:38:53 +0200

    Dump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requires = ['pyyaml>=3.1', 'colorama>=0.2.5', 'Jinja2>=2.7.1', 'MarkupSafe>=0.18
             'argcomplete>=0.6.3', 'argparse>=1.2.1', 'wsgiref>=0.1.2', 'argh==0.23.3']
 
 setup(name="lithum",
-      version="0.2.0.1",
+      version="0.2.1",
       platforms='any',
       packages = find_packages(),
       include_package_data=True,
