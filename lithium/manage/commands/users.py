4d91320c697913622f0687548b80fc7d19aeb5c0
vtemian <vladtemian@gmail.com>
2013-12-13 14:13:20 +0200

    Import user from yml

--- lithium/manage/commands/users.py
+++ lithium/manage/commands/users.py
@@ -1,18 +1,24 @@
 import os
 import uuid
 
+import yaml
+import redis
 from argh.decorators import arg
 from colorama import Fore
-import yaml
 
 @arg('file', help="Import a list of clients from a yml file into redis")
 def import_data(file):
   #TODO: check for yaml_file
   with open(file) as yaml_file:
-    clients = yaml_file.read()
+    clients = yaml.load(yaml_file.read())
 
-    redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+    redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
     for client in clients:
-      hash = 'apqu:permissions:%s:%s' % (client['client_id'], client['client_key'])
-      #redis.hset( )
+      for username in client:
+        data = client[username]
+        data['id'] = '%s' % uuid.uuid4()
+        del data['permissions']
+
+        redis_client.hmset('apqu:users:%s' % username, data)
+        redis_client.set('apqu:users:by_id:%s' % data['id'], username)
